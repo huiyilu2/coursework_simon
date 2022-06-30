d449426e9e4b2e54213d6d73d3dd0af1e14fc7d5
Veronica, Huiyi Lu <32100975+huiyilu2@users.noreply.github.com>
2022-06-29 23:47:04 -0400

    Delete case 3.R

--- case 3.R
+++
@@ -1,6 +0,0 @@
-Facebook.data<- read.csv("C:/Users/odyss/Downloads/HW2 Data_2021.csv")
-Facebook.data <- Facebook.data[,-2]
-fb.reg <- lm(clickPerDollar ~ factor(adType)+ factor(category) + factor(placement) + factor(keywords)+ factor(body) + ageMean, data = Facebook.data)
-summary(fb.reg)
-coef(fb.reg)
-
